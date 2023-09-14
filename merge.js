bfa0c96f0bbce8e9c17da74a2ea56e182345fd84
Kaito <107011546+kaito808@users.noreply.github.com>
2023-09-14 13:35:28 -1000

    Add files via upload

---
+++ merge.js
@@ -0,0 +1,44 @@
+
+function merge(arr1, arr2) {
+    let mergedArray = [];
+    let i = 0;
+    let j = 0;
+  
+    while (i < arr1.length && j < arr2.length) {
+      if (arr1[i] < arr2[j]) {
+        mergedArray.push(arr1[i]);
+        i++;
+      } else {
+        mergedArray.push(arr2[j]);
+        j++;
+      }
+    }
+  
+    while (i < arr1.length) {
+      mergedArray.push(arr1[i]);
+      i++;
+    }
+  
+    while (j < arr2.length) {
+      mergedArray.push(arr2[j]);
+      j++;
+    }
+  
+    return mergedArray;
+  }
+  
+  // Merge sort function
+  function mergeSort(arr) {
+    if (arr.length <= 1) {
+      return arr;
+    }
+  
+    const mid = Math.floor(arr.length / 2);
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
+  
+    return merge(left, right);
+  }
+  
+  module.exports = { merge, mergeSort}
+  
